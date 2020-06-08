96496d78f8547476f9e3382d4d9a848c41d0c16e
Matthew B White <whitemat@uk.ibm.com>
2020-06-08 14:47:18 +0100

    Node.js module
    Signed-off-by: Matthew B White <whitemat@uk.ibm.com>

---
+++ fabric-ledger-protos-node/index.d.ts
@@ -0,0 +1,11 @@
+/*
+ Copyright 2018 IBM All Rights Reserved.
+
+ SPDX-License-Identifier: Apache-2.0
+
+*/
+
+import * as contract_messages from './contract_messages_pb'
+import * as ledger_messages from './ledger_messages_pb'
+
+export  { contract_messages, ledger_messages };
